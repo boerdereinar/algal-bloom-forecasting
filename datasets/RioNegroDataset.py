7b05a03894e442304260271ef8782036b399571c
Einar de Gruyl <einardegruyl@gmail.com>
2022-12-08 20:22:39 +0100

    Add normalization.

--- datasets/RioNegroDataset.py
+++ datasets/RioNegroDataset.py
@@ -2,13 +2,15 @@ import os.path
 import threading
 import time
 from datetime import timedelta, datetime
-from typing import Dict, Any
+from typing import Dict, Any, Callable
 
 import torch
 from torchgeo.datasets import GeoDataset, BoundingBox
+from torchvision.transforms import Compose
 from tqdm import tqdm
 
 from edegruyl.datasets import BiologicalDataset
+from edegruyl.transforms import Normalize
 
 
 class RioNegroDataset(GeoDataset):
@@ -19,6 +21,7 @@ class RioNegroDataset(GeoDataset):
         super().__init__()
         self.window_size = window_size
         self.prediction_horizon = prediction_horizon
+        self.normalize = Normalize(torch.tensor([8.74785, 240.943, 72.4036]))
 
         # Load the datasets
         self.load_datasets(root, reservoir)
@@ -53,14 +56,20 @@ class RioNegroDataset(GeoDataset):
 
     def load_biological_unprocessed(self, pbar: tqdm, root: str, reservoir: str):
         pbar.set_description("Unprocessed biological dataset")
-        self.biological_unprocessed = BiologicalDataset(os.path.join(root, "biological", reservoir))
+        self.biological_unprocessed = BiologicalDataset(
+            os.path.join(root, "biological", reservoir),
+            transforms=Compose([self.normalize])
+        )
 
     def load_biological_processed(self, pbar: tqdm, root: str, reservoir: str):
         pbar.set_description("Processed biological dataset")
-        self.biological_processed = BiologicalDataset(os.path.join(root, "biological_processed", reservoir))
+        self.biological_processed = BiologicalDataset(
+            os.path.join(root, "biological_processed", reservoir),
+            transforms=Compose([self.normalize])
+        )
 
     def __getitem__(self, query: BoundingBox) -> Dict[str, Any]:
-        ground_truth = self.biological_unprocessed[query]["image"][0]
+        ground_truth = self.biological_unprocessed[query]["image"][1]
 
         mint = datetime.fromtimestamp(query.mint).replace(hour=0, minute=0, second=0, microsecond=0)
         maxt = datetime.fromtimestamp(query.maxt).replace(hour=23, minute=59, second=59, microsecond=999999)
