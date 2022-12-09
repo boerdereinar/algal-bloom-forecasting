478900c4c4bedab0379e30b7940088c8b7567a96
Einar de Gruyl <einardegruyl@gmail.com>
2022-12-09 19:40:16 +0100

    Different parallel implementation of interpolate because the initial implementation didn't work on the cluster.

--- preprocessing/InterpolatePreprocessor.py
+++ preprocessing/InterpolatePreprocessor.py
@@ -1,20 +1,17 @@
 import glob
 import os
-import queue
 import re
 from abc import ABC, abstractmethod
 from argparse import ArgumentParser
-from collections import deque
-from concurrent.futures import ThreadPoolExecutor
+from collections import deque, defaultdict
 from datetime import datetime, timedelta
 from enum import Enum
 from functools import reduce
-from itertools import groupby
-from typing import Any, Tuple, Dict, Iterator, Sequence
+from typing import Any, Tuple, List
 
 import numpy as np
 import rasterio
-from rasterio.io import DatasetReader, DatasetWriter
+from joblib import Parallel, delayed
 from tqdm import tqdm
 
 from edegruyl.preprocessing import Preprocessor
@@ -27,11 +24,17 @@ class InterpolationStrategy(Enum):
         return self.name
 
 
+class File:
+    def __init__(self, file: str, dataset: str, date: datetime):
+        self.file = file
+        self.dataset = dataset
+        self.date = date
+
+
 class InterpolatePreprocessor(Preprocessor):
     """Preprocessor for interpolating missing data."""
 
-    filename_regex = re.compile(r"^.*[\\/](?P<reservoir>.+)[\\/]"
-                                r"(?P<pre>.+_)(?P<date>\d{8})T\d{6}(?P<post>_.+\.tif)$")
+    filename_regex = re.compile(r"^.*[\\/](?P<reservoir>.+)_(?P<date>\d{8})T\d{6}_(?P<dataset>.+\.tif)$")
     date_format = "%Y%m%d"
     strategy: "Strategy"
 
@@ -40,11 +43,11 @@ class InterpolatePreprocessor(Preprocessor):
             source_dir: str,
             target_dir: str,
             interpolation_strategy: InterpolationStrategy,
-            max_workers: int,
-            **kwargs
+            num_workers: int = None,
+            **kwargs: Any
     ) -> None:
         super().__init__(source_dir, target_dir)
-        self.max_workers = max_workers
+        self.num_workers = num_workers
 
         match interpolation_strategy:
             case InterpolationStrategy.LookBack:
@@ -59,96 +62,74 @@ class InterpolatePreprocessor(Preprocessor):
                                    choices=list(InterpolationStrategy),
                                    help="The strategy used to interpolate the data.\nAvailable strategies:\n" +
                                         "\n".join(f"\t{v.name}: {v.value}" for v in InterpolationStrategy))
-        parent_parser.add_argument("--max-workers", type=int, default=100,
-                                   help="The maximum number of worker threads used for writing the files.")
+        parent_parser.add_argument("--num-workers", type=int, default=None,
+                                   help="The number of workers to use to process the data with.")
         return parent_parser
 
     def preprocess(self) -> None:
         """Preprocess the datasets."""
-        with ThreadPoolExecutor(max_workers=self.max_workers) as executor:
-            # Group files by reservoir
-            reservoirs = {x: list(xs) for x, xs in groupby(
-                ((f, self.filename_regex.match(f)) for f in glob.glob(self.source_dir, recursive=True)),
-                lambda f: f[1]["reservoir"])}
+        reservoirs = defaultdict(list[File])
+        for file in glob.iglob(self.source_dir, recursive=True):
+            match = self.filename_regex.match(file)
+            if match:
+                reservoirs[match["reservoir"]].append(File(
+                    file,
+                    match["dataset"],
+                    datetime.strptime(match["date"], self.date_format)
+                ))
 
-            total = 0
-            for x in reservoirs.values():
-                t0 = datetime.strptime(x[0][1]["date"], self.date_format)
-                t1 = datetime.strptime(x[-1][1]["date"], self.date_format)
-                total += (t1 - t0).days + 1
+        Parallel(self.num_workers)(
+            delayed(self._preprocess_reservoir)(item, i) for i, item in enumerate(reservoirs.items())
+        )
 
-            pbar = tqdm(total=total)
+    def _preprocess_reservoir(self, item: Tuple[str, List[File]], idx: int):
+        """Preprocess a single reservoir."""
+        reservoir, files = item
 
-            # Loop through all reservoirs
-            for reservoir, files in reservoirs.items():
-                # Create directory
-                os.makedirs(os.path.join(self.target_dir, reservoir), exist_ok=True)
+        pbar = tqdm(
+            total=(files[-1].date - files[0].date).days + 1,
+            desc=reservoir.capitalize(),
+            unit="day",
+            position=idx
+        )
 
-                data_prev = None
-                t_prev = None
+        # Create directory if it does not exist
+        os.makedirs(os.path.join(self.target_dir, reservoir), exist_ok=True)
 
-                for match, data_cur, descriptions, profile in self._load_files(executor, files):
-                    # Get the date
-                    t_cur = datetime.strptime(match["date"], self.date_format)
+        t_prev = None
+        data_prev = None
 
-                    if data_prev is None:
-                        # Handle first file in the reservoir
-                        self.strategy.first(data_cur)
-                        # Target filename
-                        target_file = os.path.join(
-                            self.target_dir,
-                            reservoir,
-                            f"{match['pre']}{t_cur.strftime(self.date_format)}{match['post']}"
-                        )
-                        # Save file in a new worker thread
-                        executor.submit(self._write_file, target_file, profile, data_cur, descriptions)
+        for file in files:
+            target_file_template = os.path.join(self.target_dir, reservoir, f"{reservoir}_%s_{file.dataset}.tif")
+
+            with rasterio.open(file.file) as src:
+                data = src.read()
+
+                if data_prev is None:
+                    pbar.set_postfix({"date": file.date.strftime(self.date_format)})
+                    # Handle first file in the reservoir
+                    self.strategy.first(data)
+                    target_file = target_file_template % file.date.strftime(self.date_format)
+
+                    # Write the original data
+                    with rasterio.open(target_file, "w", **src.profile) as target:
+                        target.write(data)
+
+                    # Update progress
+                    pbar.update(1)
+                else:
+                    for t in (t_prev + timedelta(days) for days in range(1, (file.date - t_prev).days + 1)):
+                        pbar.set_postfix({"date": t.strftime(self.date_format)})
+                        interpolated_data = self.strategy.interpolate(data_prev, t_prev, data, file.date, t)
+                        target_file = target_file_template % t.strftime(self.date_format)
+
+                        # Write the interpolated data
+                        with rasterio.open(target_file, "w", **src.profile) as target:
+                            target.write(interpolated_data)
                         pbar.update(1)
-                    else:
-                        # Process all missing samples including the last sample
-                        for t_i in [t_prev + timedelta(days=dt) for dt in range(1, (t_cur - t_prev).days + 1)]:
-                            pbar.set_description(f"{reservoir} {t_i:%Y-%m-%d}")
-                            data = self.strategy.interpolate(data_prev, t_prev, data_cur, t_cur, t_i)
-                            # Target filename
-                            target_file = os.path.join(
-                                self.target_dir,
-                                reservoir,
-                                f"{match['pre']}{t_i.strftime(self.date_format)}{match['post']}"
-                            )
-                            # Save file in a new worker thread
-                            executor.submit(self._write_file, target_file, profile, data, descriptions)
-                            pbar.update(1)
 
-                    # Update previous sample
-                    data_prev = data_cur
-                    t_prev = t_cur
-
-    def _load_files(self, executor: ThreadPoolExecutor, files) -> Iterator[Tuple[re.Match, np.ndarray, Dict[str, Any]]]:
-        """Prefetch files."""
-        prefetched = queue.SimpleQueue()
-
-        files_iterator = iter(files)
-        for _ in range(20):
-            file = next(files_iterator, None)
-            if not file:
-                break
-            prefetched.put(executor.submit(self._read_file, file[0], file[1]))
-
-        while not prefetched.empty():
-            yield prefetched.get(True).result()
-            file = next(files_iterator, None)
-            if file:
-                prefetched.put(executor.submit(self._read_file, file[0], file[1]))
-
-    @staticmethod
-    def _read_file(file: str, match: re.Match) -> Tuple[re.Match, np.ndarray, Sequence[str], Dict[str, Any]]:
-        with rasterio.open(file) as src:  # type: DatasetReader
-            return match, src.read(), src.descriptions, src.profile
-
-    @staticmethod
-    def _write_file(file: str, profile: Dict[str, Any], data: np.ndarray, descriptions: Sequence[str]) -> None:
-        with rasterio.open(file, "w", **profile) as target:  # type: DatasetWriter
-            target.write(data)
-            target.descriptions = descriptions
+                t_prev = file.date
+                data_prev = data
 
 
 class Strategy(ABC):
