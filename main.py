78f061a0d611a855642a889dfc970545a019994c
Einar de Gruyl <einardegruyl@gmail.com>
2022-12-16 19:13:41 +0100

    Possible fix models not saving in the correct directory.

--- main.py
+++ main.py
@@ -72,7 +72,11 @@ def train(args: Namespace, unknown_args: List[str]) -> None:
 
     # Logging
     lr_monitor = LearningRateMonitor("step", True)
-    wandb_logger = WandbLogger(project="algal-bloom", log_model=True)
+    wandb_logger = WandbLogger(
+        project="algal-bloom",
+        log_model=True,
+        save_dir="." if trainer_args.default_root_dir is None else trainer_args.default_root_dir
+    )
 
     # Trainer
     trainer: Trainer = Trainer.from_argparse_args(
