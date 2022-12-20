d9d40eabadfbd89989bea6beade3646f57ac79e3
Einar de Gruyl <einardegruyl@gmail.com>
2022-12-20 19:20:37 +0100

    Implement cross channel interpolation layer.

--- models/InterpolationLayer.py
+++ models/InterpolationLayer.py
@@ -1,18 +1,27 @@
+from typing import Optional, Tuple
+
 import torch
+from torch import Tensor
 from torch.nn import Module, Parameter
 
 
 class SingleChannelInterpolation(Module):
-    def __init__(self, window_size: int, prediction_horizon: int, reconstruction: bool = False):
+    def __init__(
+            self,
+            window_size: int,
+            prediction_horizon: int,
+            reconstruction: bool = False,
+            kappa: float = 10
+    ) -> None:
         super().__init__()
 
-        self.kappa = 10.0
+        self.kappa = kappa
         self.window_size = window_size
         self.prediction_horizon = prediction_horizon
         self.reconstruction = reconstruction
         self.kernel = Parameter(torch.zeros((self.window_size,)))
 
-    def forward(self, x_t, d, m):
+    def forward(self, x_t: Tensor, d: Tensor, m: Tensor) -> Tuple[Tensor, Tensor] | Tuple[Tensor, Tensor, Tensor]:
         if self.reconstruction:
             output_dim = self.window_size
             ref_t = d.unsqueeze(2).expand(-1, -1, output_dim, -1)
@@ -32,12 +41,46 @@ class SingleChannelInterpolation(Module):
         y = torch.einsum("ijkl,ijkl->ijl", w1, x_t)
 
         if self.reconstruction:
-            rep1 = torch.cat([y, w], 1)
-        else:
-            w_t = torch.logsumexp(-self.kappa * alpha * norm + torch.log(m), dim=2)
-            w_t = w_t.unsqueeze(2).expand(-1, -1, self.window_size, -1)
-            w_t = torch.exp(-self.kappa * alpha * norm + torch.log(m) - w_t)
-            y_trans = torch.einsum("ijkl,ijkl->ijl", w_t, x_t)
-            rep1 = torch.cat([y, w, y_trans], 1)
+            return y, w
 
-        return rep1
+        w_t = torch.logsumexp(-self.kappa * alpha * norm + torch.log(m), dim=2)
+        w_t = w_t.unsqueeze(2).expand(-1, -1, self.window_size, -1)
+        w_t = torch.exp(-self.kappa * alpha * norm + torch.log(m) - w_t)
+        y_trans = torch.einsum("ijkl,ijkl->ijl", w_t, x_t)
+        return y, w, y_trans
+
+
+class CrossChannelInterpolation(Module):
+    def __init__(
+            self,
+            n_features: int,
+            window_size: int,
+            prediction_horizon: int,
+            reconstruction: bool = False
+    ) -> None:
+        super().__init__()
+
+        self.n_features = n_features
+        self.window_size = window_size
+        self.prediction_horizon = prediction_horizon
+        self.reconstruction = reconstruction
+        self.cross_channel_interpolation = Parameter(torch.eye(self.n_features))
+
+    def forward(self, y: Tensor, w: Tensor, y_trans: Optional[Tensor] = None) -> Tensor | Tuple[Tensor, Tensor, Tensor]:
+        intensity = torch.exp(w)
+        y = y.permute(0, 2, 1)
+        w = w.permute(0, 2, 1)
+        w2 = w
+        w = w.unsqueeze(-1).expand(-1, -1, -1, self.n_features)
+        den = w.logsumexp(dim=2)
+        w = torch.exp(w2 - den)
+        mean = y.mean(dim=1)
+        mean = mean[:, None, :].expand(-1, self.prediction_horizon, -1)
+        w2 = (w * (y - mean)) @ self.cross_channel_interpolation + mean
+        rep1 = w2.permute(0, 2, 1)
+
+        if self.reconstruction:
+            return rep1
+
+        y_trans = y_trans - rep1
+        return rep1, intensity, y_trans
