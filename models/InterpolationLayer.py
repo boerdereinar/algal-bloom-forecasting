15ea3c40f37e6ce5ddc6c1bb346957028266b8eb
Einar de Gruyl <einardegruyl@gmail.com>
2022-12-27 18:17:43 +0100

    Add proper forward method to InterpolationNetwork that reshapes and masks the input tensors and feeds those tensors to the interpolation network. Add optional return of observed values to extract_batch method.

--- models/InterpolationLayer.py
+++ models/InterpolationLayer.py
@@ -1,5 +1,8 @@
+from typing import Literal, Tuple, overload
+
 import torch
 from torch import Tensor, nn
+from torch.nn.functional import mse_loss
 
 
 class InterpolationNetwork(nn.Module):
@@ -10,42 +13,161 @@ class InterpolationNetwork(nn.Module):
         num_features (int): The number of features in the input data.
         observed_points (int): The number of observed points.
         reference_points (int): The number of reference points.
-        reconstruction (bool, optional): Flag indicating whether the network is used for reconstruction.
-            If set to True, the output will only be the interpolated points, whereas if set to False,
-            the output will also include the transformed points. Default: False.
     """
     def __init__(
             self,
             num_features,
             observed_points,
-            reference_points,
-            reconstruction=False
+            reference_points
     ) -> None:
         super().__init__()
 
-        self.interpolation = nn.Sequential(
-            SingleChannelInterpolation(observed_points, reference_points, reconstruction),
+        self.num_features = num_features
+        self.observed_points = observed_points
+        self.reference_points = reference_points
+
+        self.interpolate = nn.Sequential(
+            SingleChannelInterpolation(observed_points, reference_points),
             nn.Sigmoid(),
-            CrossChannelInterpolation(num_features, observed_points, reference_points, reconstruction),
+            CrossChannelInterpolation(num_features, observed_points, reference_points),
             nn.Sigmoid()
         )
 
-    def forward(self, x: Tensor) -> Tensor:
-        """
-        Interpolate the input data.
+        self.reconstruct = nn.Sequential(
+            SingleChannelInterpolation(observed_points, reference_points, True),
+            nn.Sigmoid(),
+            CrossChannelInterpolation(num_features, observed_points, reference_points, True),
+            nn.Sigmoid()
+        )
 
-        Parameters:
-            x (Tensor): The input data with shape (3, batch_size, num_features, observed_points) where:
-                x[0] is the data with missing values.
-                x[1] is the timestamps corresponding to each value in x[0].
-                x[2] is the mask of all observed values where 1 denotes an observed value and 0 denotes a missing value.
+    @overload
+    def forward(
+            self,
+            x: Tensor,
+            water_mask: Tensor,
+            observed_x: Tensor,
+            masked: bool = False,
+            return_reconstructed: Literal[False] = False
+    ) -> Tuple[Tensor, Tensor]:
+        """
+        Interpolate and reconstruct the input data.
+
+        Args:
+            x: The images tensor of shape (batch, observed_points, num_features, height, width)
+            water_mask: The water mask tensor of shape (batch, height, width)
+            observed_x: A tensor of shape (batch_size, observed_points, num_features, height, width) indicating which
+                values in the images tensor are observed (not NaN).
+            masked: Whether the interpolation is only applied to the area masked by the water_mask.
+            return_reconstructed: Whether to return the reconstructed Tensor.
 
         Returns:
-            Tensor: The interpolated points. If reconstruction is set to True,
-                the output will have shape (batch_size, num_features, reference_points).
-                Otherwise, the output will have shape (3, batch_size, num_features, reference_points).
+            A tuple containing:
+            The interpolated tensor features of shape (batch_size, 3, num_features, reference_points, height, width).
+            The mse loss of the reconstructed tensor.
         """
-        return self.interpolation(x)
+        ...
+
+    @overload
+    def forward(
+            self,
+            x: Tensor,
+            water_mask: Tensor,
+            observed_x: Tensor,
+            masked: bool = False,
+            return_reconstructed: Literal[True] = True
+    ) -> Tuple[Tensor, Tensor, Tensor]:
+        """
+        Interpolate and reconstruct the input data.
+
+        Args:
+            x: The images tensor of shape (batch_size, observed_points, num_features, height, width)
+            water_mask: The water mask tensor of shape (batch_size, height, width)
+            observed_x: A tensor of shape (batch_size, observed_points, num_features, height, width) indicating which
+                values in the images tensor are observed (not NaN).
+            masked: Whether the interpolation is only applied to the area masked by the water_mask.
+            return_reconstructed: Whether to return the reconstructed Tensor.
+
+        Returns:
+            A tuple containing:
+            The interpolated tensor features of shape (batch_size, 3, num_features, reference_points, height, width).
+            The mse loss of the reconstructed tensor.
+            The reconstructed images tensor with the same shape as the images tensor.
+        """
+        ...
+
+    def forward(
+            self,
+            x: Tensor,
+            water_mask: Tensor,
+            observed_x: Tensor,
+            masked: bool = False,
+            return_reconstructed: bool = False
+    ) -> Tuple[Tensor, Tensor] | Tuple[Tensor, Tensor, Tensor]:
+        batch_size, observed_points, num_features, height, width = x.shape
+
+        assert observed_points == self.observed_points
+        assert num_features == self.num_features
+
+        masked_indices = None
+
+        # Reshapes the input tensors
+        # x_t: (n, num_features, observed_points)
+        # d:   (n, num_features, observed_points)
+        # m:   (n, num_features, observed_points)
+        # n:   (batch_size * height * width) if masked is False, (batch_size * masked area) otherwise.
+        if masked:
+            masked_indices = torch.where(water_mask)
+            x_t = x.permute(0, 3, 4, 2, 1)[masked_indices]
+            d = torch.arange(observed_points).to(x)
+            d = d[None, None, :].expand(*x_t.shape)
+            m = observed_x.permute(0, 3, 4, 2, 1)[masked_indices]
+        else:
+            x_t = x.permute(0, 3, 4, 2, 1).reshape(-1, num_features, observed_points)
+            d = torch.arange(observed_points).to(x)
+            d = d[None, None, :].expand(*x_t.shape)
+            m = observed_x.permute(0, 3, 4, 2, 1)
+            m = m.reshape(-1, num_features, observed_points)
+
+        # Feed the result into the interpolation network
+        # This returns a tensor of shape (3, n, num_features, reference_points)
+        x_in = torch.stack((x_t, d, m))
+        interp_out = self.interpolate(x_in)
+
+        # Reshapes the interpolated tensor to (batch_size, reference_points, 3, num_features, height, width)
+        if masked:
+            y = torch.zeros((batch_size, height, width, 3, num_features, self.reference_points))
+            y[masked_indices] = interp_out.permute(1, 0, 2, 3)
+            y = y.permute(0, 5, 3, 4, 1, 2)
+        else:
+            y = interp_out.view(3, batch_size, height, width, num_features, self.reference_points)
+            y = y.permute(1, 5, 0, 4, 2, 3)
+
+        # Hold out 20% of the input data for reconstruction
+        m_holdout = (torch.rand_like(m) > 0.2) & m
+
+        # Feed the result into the reconstruction network
+        # This returns a tensor of shape (n, num_features, observed_points)
+        x_in = torch.stack((x_t, d, m_holdout))
+        reconst_out = self.reconstruct(x_in)
+
+        # Get which values were held out and compute the loss
+        held_out = ~m_holdout & m
+        mse = mse_loss(reconst_out[held_out], x_t[held_out])
+
+        # Return the reconstructed images tensor.
+        if return_reconstructed:
+            # Reshapes the reconstructed tensor to (batch_size, observed_points, num_features, height, width)
+            if masked:
+                y_reconst = torch.zeros((batch_size, height, width, num_features, observed_points))
+                y_reconst[masked_indices] = reconst_out
+                y_reconst = y_reconst.permute(0, 4, 3, 1, 2)
+            else:
+                y_reconst = reconst_out.view(batch_size, height, width, num_features, observed_points)
+                y_reconst = y_reconst.permute(0, 4, 3, 1, 2)
+
+            return y, mse, y_reconst
+
+        return y, mse
 
 
 class SingleChannelInterpolation(nn.Module):
--- utils/modelutils.py
+++ utils/modelutils.py
@@ -1,36 +1,84 @@
-from typing import Dict, Tuple
+from typing import Dict, Literal, Tuple, overload
 
 from torch import Tensor
 
 
-def extract_batch(batch: Dict[str, Tensor]) -> Tuple[Tensor, Tensor, Tensor, Tensor]:
+@overload
+def extract_batch(
+        batch: Dict[str, Tensor],
+        mask_input: Literal[False] = False
+) -> Tuple[Tensor, Tensor, Tensor, Tensor]:
     """Extracts and processes tensors from a batch of data.
 
     Args:
         batch: A dictionary containing keys "images", "ground_truth", and "water_mask".
-            "images": A tensor of shape (batch_size, window_size, channels, height, width) representing the images in
+            "images": A tensor of shape (batch_size, window_size, bands, height, width) representing the images in
                 the batch.
             "ground_truth": A tensor of shape (batch_size, height, width) representing the ground truth labels for
                 the images.
             "water_mask": A tensor of shape (batch_size, height, width) representing the mask applied to the images.
+        mask_input: Whether to return a tensor indicating which values in the images tensor are observed (not NaN).
 
     Returns:
-        A tuple containing the processed tensors (x, y, m, observed).
+        A tuple containing the processed tensors (x, y, m, observed_y).
         "x": The images tensor with any NaN values replaced with zeros.
         "y": The ground truth labels tensor with any NaN values replaced with zeros.
         "m": The water mask tensor.
-        "observed": A tensor of shape (batch_size, channels, height, width) indicating which values in the ground
+        "observed_y": A tensor of shape (batch_size, height, width) indicating which values in the ground
             truth labels tensor are observed (not NaN).
     """
+    ...
+
+
+@overload
+def extract_batch(
+        batch: Dict[str, Tensor],
+        mask_input: Literal[True]
+) -> Tuple[Tensor, Tensor, Tensor, Tensor, Tensor]:
+    """Extracts and processes tensors from a batch of data.
+
+    Args:
+        batch: A dictionary containing keys "images", "ground_truth", and "water_mask".
+            "images": A tensor of shape (batch_size, window_size, bands, height, width) representing the images in
+                the batch.
+            "ground_truth": A tensor of shape (batch_size, height, width) representing the ground truth labels for
+                the images.
+            "water_mask": A tensor of shape (batch_size, height, width) representing the mask applied to the images.
+        mask_input: Whether to return a tensor indicating which values in the images tensor are observed (not NaN).
+
+    Returns:
+        A tuple containing the processed tensors (x, y, m, observed_x, observed_y).
+        "x": The images tensor with any NaN values replaced with zeros.
+        "y": The ground truth labels tensor with any NaN values replaced with zeros.
+        "m": The water mask tensor.
+        "observed_x": A tensor of shape (batch_size, window_size, bands, height, width) indicating which values in
+            the images tensor are observed (not NaN).
+        "observed_y": A tensor of shape (batch_size, height, width) indicating which values in the ground
+            truth labels tensor are observed (not NaN).
+    """
+    ...
+
+
+def extract_batch(
+        batch: Dict[str, Tensor],
+        mask_input: bool = False
+) -> Tuple[Tensor, Tensor, Tensor, Tensor, Tensor] | Tuple[Tensor, Tensor, Tensor, Tensor]:
     x = batch["images"]
     y = batch["ground_truth"]
     m = batch["water_mask"]
 
     # Observed values
-    observed = ~y.isnan()
+    observed_y = ~y.isnan()
+
+    observed_x = None
+    if mask_input:
+        observed_x = ~x.isnan()
 
     # Remove NaNs
     x = x.nan_to_num()
     y = y.nan_to_num()
 
-    return x, y, m, observed
+    if mask_input:
+        return x, y, m, observed_x, observed_y  # type: ignore
+
+    return x, y, m, observed_y
